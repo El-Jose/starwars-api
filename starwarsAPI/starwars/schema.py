65678c2e819ad6f5e0cc668cb265f38b34a90a3e
El-Jose <el-jose@protonmail.com>
2020-10-04 20:14:56 -0500

    Define queries for Characters and Planets, Add url for graphiQL

--- starwarsAPI/starwars/schema.py
+++ starwarsAPI/starwars/schema.py
@@ -1,12 +1,39 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from .models import Link
+from .models import Character, Planet, Producer, Movie
 
 
-class LinkType(DjangoObjectType):
-    pass
+class CharacterType(DjangoObjectType):
+    class Meta:
+        model = Character
+        fields = "__all__"
+
+
+class PlanetType(DjangoObjectType):
+    class Meta:
+        model = Planet
+        fields = "__all__"
+
+
+class ProducerType(DjangoObjectType):
+    class Meta:
+        model = Producer
+        fields = "__all__"
+
+
+class MovieType(DjangoObjectType):
+    class Meta:
+        model = Movie
+        fields = "__all__"
 
 
 class Query(graphene.ObjectType):
-    pass
+    characters = graphene.List(CharacterType)
+    planets = graphene.List(PlanetType)
+
+    def resolve_characters(self, info, **kwargs):
+        return Character.objects.all()
+
+    def resolve_planets(self, info, **kwargs):
+        return Planet.objects.all()
